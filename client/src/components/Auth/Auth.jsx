2c3d510d124bf89364fdbcea0b2ff31a627409cd
matthew-hocking <matthocking2439@gmail.com>
2022-06-01 14:44:20 +1200

    clearning app layout and initialzing auth feature

---
+++ client/src/components/Auth/Auth.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core'
+
+import useStyles from './styles'
+
+const Auth = () => {
+  return (
+    <div>Auth</div>
+  )
+}
+
+export default Auth
