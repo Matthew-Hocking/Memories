96a22e56d0056f0b9435d07ce56a33fba6c5dbba
matthew-hocking <matthocking2439@gmail.com>
2022-06-08 15:09:01 +1200

    reset init

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -18,17 +18,17 @@ const Home = () => {
 
   return (
     <Grow in>
-          <Container>
-            <Grid container className={ classes.mainContainer } justifyContent="space-between" alignItems="stretch" spacing={3}>
-              <Grid item xs={12} sm={7}>
-                <Posts setCurrentId={setCurrentId} />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <Form currentId={currentId} setCurrentId={setCurrentId} />
-              </Grid>
-            </Grid>
-          </Container>
-        </Grow>
+      <Container>
+        <Grid container className={ classes.mainContainer } justifyContent="space-between" alignItems="stretch" spacing={3}>
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
   )
 }
 
